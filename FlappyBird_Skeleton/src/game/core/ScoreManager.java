b48e55517372c714209a9a56ebfca84a671b7832
Joser404 <heyhema94@gmail.com>
2026-05-23 03:11:49 +0700

    Update ScoreManager

--- FlappyBird_Skeleton/src/game/core/ScoreManager.java
+++ FlappyBird_Skeleton/src/game/core/ScoreManager.java
@@ -1,38 +1,41 @@
 package game.core;
 
-/**
- * Đếm điểm + lưu high score.
- * Mỗi tick gọi increment() — score sẽ tăng đều theo thời gian sống.
- */
+import game.entities.Bird;
+import game.entities.Pipe;
+
+import java.util.List;
+
 public class ScoreManager implements Resettable {
 
-    // TODO: 2 field score và highScore (kiểu double để tăng từ từ)
     private static final double SCORE_INCREMENT = 0.01;
+    private double score;
+    private double highScore;
 
     public ScoreManager() {
-        // TODO: init score = 0, highScore = 0
+        this.score = 0;
+        this.highScore = 0;
     }
 
     public void increment() {
-        // TODO: score += SCORE_INCREMENT;
+        score++;
     }
 
     public void updateHighScore() {
-        // TODO: nếu score > highScore thì gán highScore = score
+        if (score > highScore) {
+            highScore = score;
+        }
     }
 
     @Override
     public void reset() {
-        // TODO: chỉ reset score (giữ highScore)
+        score = 0;
     }
 
     public int getScore() {
-        // TODO: return (int) score;
-        return 0;
+        return (int) score;
     }
 
     public int getHighScore() {
-        // TODO
-        return 0;
+        return (int) highScore;
     }
 }
